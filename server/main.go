894993f738ce1404eae11c04a78fdb3e0ee70dd3
lmx <1721261216@qq.com>
2022-03-28 09:54:36 +0800

    feature ： completed the gin layer server(get,put and delete)

---
+++ server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+
+	kvPath := flag.String("k", "kv", "comma kv storage path")
+	raftPath := flag.String("r", "raft", "comma raft storage path")
+	addr := flag.String("a", ":8080", "comma port")
+	flag.Parse()
+
+	kve := newKVEngine(*kvPath, *raftPath)
+	defer kve.kve.Close()
+	ge := router(kve)
+	ge.Run(*addr)
+}
