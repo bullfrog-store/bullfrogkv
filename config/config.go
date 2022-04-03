9c58830056ecbeaede27715e4e55295a0e8caf25
qiuyiliang.ok <qiuyiliang.ok@bytedance.com>
2022-04-03 19:50:35 +0800

    feature: add config struct

--- config/config.go
+++ config/config.go
@@ -1,1 +1,21 @@
 package config
+
+type Config struct {
+	RouteConfig RouteConfig
+	StoreConfig StoreConfig
+	RaftConfig  RaftConfig
+}
+
+type RouteConfig struct {
+	Addr string
+}
+
+type StoreConfig struct {
+	StoreId  uint64
+	DataPath string
+}
+
+type RaftConfig struct {
+	LogGCCountLimit    int
+	CompactCheckPeriod int
+}
